8a68de14ff89e34fa67a8178d3b12e084d8fed60
Amarilis Khairina Mardhiyah <56585133+amariliskm@users.noreply.github.com>
2023-08-29 07:19:35 +0700

    Time: 2982 ms (66.64%), Space: 0B (100%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -1,14 +1,13 @@
-# Write your MySQL query statement below
-SELECT t.name
-FROM (
-    SELECT name,sales_id
+WITH cte AS (
+  SELECT name,sales_id
     FROM SalesPerson
-    HAVING sales_id NOT IN(
-#         # cari sales yang pernah layanin company 'RED'
+    WHERE sales_id NOT IN(
+        # cari sales yang pernah layanin company 'RED'
         SELECT o.sales_id
         FROM Orders o
         LEFT JOIN Company c
         ON o.com_id = c.com_id
-        WHERE c.name = 'RED'
-    )
-) t;
+        WHERE c.name = 'RED')
+)
+SELECT name
+FROM cte
