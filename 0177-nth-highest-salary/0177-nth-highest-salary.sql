0fe9961a669bbfb337f2a79d2f6ac8c6f80108fd
Amarilis Khairina Mardhiyah <56585133+amariliskm@users.noreply.github.com>
2023-08-30 08:29:47 +0700

    Time: 746 ms (72.62%), Space: 0B (100%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -5,8 +5,9 @@ DECLARE M INT;
 SET M=N-1;
   RETURN (
       # Write your MySQL query statement below.
-      SELECT DISTINCT salary AS getNthHighestSalary
+      SELECT salary AS getNthHighestSalary
       FROM Employee
+      GROUP BY salary
       ORDER BY salary DESC
       LIMIT M,1   
   );
