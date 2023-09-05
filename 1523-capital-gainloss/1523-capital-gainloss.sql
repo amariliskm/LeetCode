b9cd5d9e09b8c9aee15ba513a77f966708d32a5f
Amarilis Khairina Mardhiyah <56585133+amariliskm@users.noreply.github.com>
2023-09-05 18:05:41 +0700

    Time: 1880 ms (5.02%), Space: 0B (100%) - LeetHub

--- 1523-capital-gainloss/1523-capital-gainloss.sql
+++ 1523-capital-gainloss/1523-capital-gainloss.sql
@@ -1,15 +1,21 @@
-    SELECT t1.stock_name, sell_price - buy_price AS capital_gain_loss 
-    FROM (
-      SELECT stock_name, operation, SUM(price) as buy_price
-      FROM Stocks T1
-      WHERE operation = 'Buy'
-      GROUP BY stock_name, operation 
-    )t1    
-    LEFT JOIN (
-      SELECT stock_name, operation, SUM(price) as sell_price
-      FROM Stocks
-      WHERE operation = 'Sell'
-      GROUP BY stock_name, operation
-    )t2
-      ON t1.stock_name = t2.stock_name
+select 
+	stock_name,
+	sum(case when operation = 'Sell' then price else -price end) as capital_gain_loss
+from Stocks
+group by stock_name
+
+    # SELECT t1.stock_name, sell_price - buy_price AS capital_gain_loss 
+    # FROM (
+    #   SELECT stock_name, operation, SUM(price) as buy_price
+    #   FROM Stocks T1
+    #   WHERE operation = 'Buy'
+    #   GROUP BY stock_name, operation 
+    # )t1    
+    # LEFT JOIN (
+    #   SELECT stock_name, operation, SUM(price) as sell_price
+    #   FROM Stocks
+    #   WHERE operation = 'Sell'
+    #   GROUP BY stock_name, operation
+    # )t2
+    #   ON t1.stock_name = t2.stock_name
 
